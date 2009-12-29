ff12be7f01618e4b20bd36f9f33c74534874f1b5
zackdesign <zackdesign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-29 01:38:14 +0000

    CSS Dropdown Menu 3.0.1
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-css-drop-down-menu/tags/3.0.1@187738 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js_menu.php
@@ -0,0 +1,28 @@
+<?php
+
+   /*
+
+   Plugin Name: WP CSS Dropdown Menu JS add-on
+   Plugin URI: http://www.zackdesign.biz/category/wp-plugins/css-dropdown-menu
+   Description: A complementary javascript plugin to the CSS dropdown menu. This uses a different stylesheet found in <plugin folder>/js/superfish/css/superfish.css
+   Version: 3.0.1
+   Author: Isaac Rowntree
+   Author URI: http://www.zackdesign.biz
+
+   */
+   
+   add_action('init', 'includeSuperfish');
+   
+   function includeSuperfish()
+   {
+        wp_deregister_style('wp-css-dropdown-menu-style');
+	wp_enqueue_script('superfish','/wp-content/plugins/wordpress-css-drop-down-menu/js/superfish/js/superfish.js', array('jquery'));
+	wp_enqueue_script('superfish-hover','/wp-content/plugins/wordpress-css-drop-down-menu/js/superfish/js/hoverIntent.js', array('jquery'));
+	wp_enqueue_style('superfish','/wp-content/plugins/wordpress-css-drop-down-menu/js/superfish/css/superfish.css');
+	
+	update_option('wp_css_menu_jclass', 'sf-menu');
+	
+	wp_enqueue_script('start-superfish','/wp-content/plugins/wordpress-css-drop-down-menu/js/start-superfish.js');
+   }
+   
+   ?>
